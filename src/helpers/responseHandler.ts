d630c6e5d440321ac3fee36a2db2e2ff31ca3e2c
Eoan O Dea <odeaeoan@gmail.com>
2021-06-08 15:52:58 +0200

    Initial commit, spun up ts server from boiler plate and integrated models, controllers and routes

---
+++ src/helpers/responseHandler.ts
@@ -0,0 +1,25 @@
+/**
+ * Sort data for a successful response
+ *
+ * @param data
+ */
+export const handleSuccess = (data: any) => {
+  return {
+    sucess: true,
+    data,
+  };
+};
+
+/**
+ * Sort data for a error response
+ *
+ * @param error
+ */
+export const handleError = (error: any) => {
+  const errorString = typeof error !== "string" ? error.message : error;
+
+  return {
+    sucess: false,
+    error: errorString,
+  };
+};
